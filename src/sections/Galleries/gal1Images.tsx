859c7b6e09bec11880dcc467dfe333b0622b0074
fourthstanza <kitkatmatthew@hotmail.com>
2026-02-08 17:30:13 -0500

    added dynamic image imports

--- src/sections/Galleries/gal1Images.tsx
+++ src/sections/Galleries/gal1Images.tsx
@@ -1,17 +1,8 @@
-import img1 from "@/assets/images/Gallery1/000033880006.jpg";
-import img2 from "@/assets/images/Gallery1/000033880007.jpg";
-import img3 from "@/assets/images/Gallery1/000033880010.jpg";
-import img4 from "@/assets/images/Gallery1/000033880012.jpg";
-import img5 from "@/assets/images/Gallery1/000033880016.jpg";
-import img6 from "@/assets/images/Gallery1/000033880008.jpg";
-import img7 from "@/assets/images/Gallery1/000033880009.jpg";
+const images : Record<string, {default: string}> = import.meta.glob(
+  "@/assets/images/Gallery1/*.{png,jpg,jpeg,webp,svg}",
+  { eager: true }
+);
 
-export const Gal1Images: string[] = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-]
+export const Gal1Images : string[] = Object.values(images).map(
+  mod => mod.default
+);
--- src/sections/Galleries/gal2images.tsx
+++ src/sections/Galleries/gal2images.tsx
@@ -1,24 +1,8 @@
-import img1 from "@/assets/images/Gallery2/_1760909.jpg";
-import img2 from "@/assets/images/Gallery2/_1760919.jpg";
-import img3 from "@/assets/images/Gallery2/_1760920.jpg";
-import img4 from "@/assets/images/Gallery2/_1760925.jpg";
-import img5 from "@/assets/images/Gallery2/_1760926.jpg";
-import img6 from "@/assets/images/Gallery2/_1760936.jpg";
-import img7 from "@/assets/images/Gallery2/DSC08890.jpg";
-import img8 from "@/assets/images/Gallery2/DSC08891.jpg";
-import img9 from "@/assets/images/Gallery2/DSC08893.jpg";
-import img10 from "@/assets/images/Gallery2/DSC08896.jpg";
+const images : Record<string, {default: string}> = import.meta.glob(
+  "@/assets/images/Gallery2/*.{png,jpg,jpeg,webp,svg}",
+  { eager: true }
+);
 
-export const Gal2Images: string[] = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5,
-    img6,
-    img7,
-    img7,
-    img8,
-    img9,
-    img10
-]
+export const Gal2Images : string[] = Object.values(images).map(
+  mod => mod.default
+);
